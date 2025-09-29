cd677239d02e0a5ea7dcf4c87e1b3c12a5e7d1b6
Mahendra Dinata <remdinata@gmail.com>
2025-09-29 21:14:25 +0700

    add new keybinds for toggleterm

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -3,7 +3,18 @@ require('lze').load {
   event = 'DeferredUIEnter',
   after = function()
     require('toggleterm').setup {
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 20
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.45
+        end
+      end,
       open_mapping = [[<C-\>]],
     }
   end,
+  keys = {
+    { [[<C-S-\>]], '<cmd>ToggleTerm direction=horizontal<CR>', mode = { 'n', 'i' }, desc = 'ToggleTerm Horizontal' },
+    { [[<C-A-\>]], '<cmd>ToggleTerm direction=vertical<CR>', mode = { 'n', 'i' }, desc = 'ToggleTerm Vertical' },
+  },
 }
